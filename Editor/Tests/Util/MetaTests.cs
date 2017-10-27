31fb37bbe1ec262fb2231e4911c12ff49843738e
Max J. Polster <max.polster@id-p.de>
2017-10-27 02:18:35 +0200

    Added meta class.

---
+++ Editor/Tests/Util/MetaTests.cs
@@ -0,0 +1,67 @@
+﻿
+using NUnit.Framework;
+using Unicorn.Util;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace Unicorn.Tests.Util {
+	public class MetaTests {
+
+		[Test]
+		public void Set() {
+			var meta = new Meta();
+			meta.Set(new DummyObject());
+			Assert.True(meta.Contains<DummyObject>());
+		}
+
+		[Test]
+		public void Remove() {
+			var meta = new Meta();
+			meta.Set(new DummyObject());
+			meta.Remove<DummyObject>();
+			Assert.False(meta.Contains<DummyObject>());
+		}
+
+		[Test]
+		public void UseWithFunction() {
+			var meta = new Meta();
+			meta.Set(new DummyObject() { Value = 42 });
+			Assert.True(meta.Use<DummyObject, bool>(o => o.Value > 7));
+			Assert.False(meta.Use<DummyUnityObject, bool>(o => true));
+		}
+
+		[Test]
+		public void UseWithFunctionDefault() {
+			var meta = new Meta();
+			meta.Set(new DummyObject() { Value = 42 });
+			Assert.True(meta.Use<DummyObject, bool>(o => o.Value > 7, false));
+			Assert.True(meta.Use<DummyUnityObject, bool>(o => false, true));
+		}
+
+		[Test]
+		public void Use() {
+			var meta = new Meta();
+			var obj = new DummyObject();
+			meta.Set(obj);
+			Assert.True(meta.Use<DummyObject>(o => o.Used = true));
+			Assert.True(obj.Used);
+		}
+
+		[Test]
+		public void DeadObjects() {
+			var meta = new Meta();
+			var obj = ScriptableObject.CreateInstance<DummyUnityObject>();
+			meta.Set(obj);
+			Assert.True(meta.Contains<DummyUnityObject>());
+			UnityObject.DestroyImmediate(obj);
+			Assert.False(meta.Contains<DummyUnityObject>());
+		}
+
+		public class DummyObject {
+			public bool Used;
+			public int Value;
+		}
+
+		public class DummyUnityObject : ScriptableObject { }
+	}
+}
---
+++ Library/Util/Meta.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace Unicorn.Util {
+	public class Meta {
+		public Meta() {
+			_map = new SortedDictionary<string, object>();
+		}
+
+		private readonly SortedDictionary<string, object> _map;
+
+
+
+		public void Set<T>(T value) where T : class {
+			_map[typeof(T).FullName] = value;
+		}
+
+		public void Remove<T>() {
+			_map.Remove(typeof(T).FullName);
+		}
+
+		public bool Contains<T>() where T : class {
+			object value;
+			if (_map.TryGetValue(typeof(T).FullName, out value)) {
+				if (IsDead(value)) {
+					_map.Remove(typeof(T).FullName);
+				} else {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public R Use<T, R>(Func<T, R> func, R defaultValue) where T : class {
+			object value;
+			if (_map.TryGetValue(typeof(T).FullName, out value)) {
+				if (IsDead(value)) {
+					_map.Remove(typeof(T).FullName);
+				} else {
+					return func((T)value);
+				}
+			}
+			return defaultValue;
+		}
+
+		public R Use<T, R>(Func<T, R> func) where T : class {
+			return Use(func, default(R));
+		}
+
+		public bool Use<T>(Action<T> action) where T : class {
+			object value;
+			if (_map.TryGetValue(typeof(T).FullName, out value)) {
+				if (IsDead(value)) {
+					_map.Remove(typeof(T).FullName);
+				} else {
+					action((T)value);
+					return true;
+				}
+			}
+			return false;
+		}
+
+
+
+		private static bool IsDead(object value) {
+			return (value is UnityObject) && !(UnityObject)value;
+		}
+	}
+}
