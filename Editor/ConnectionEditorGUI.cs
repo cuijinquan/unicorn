b7005323692448a21c827611c7767208552ecab0
Max J. Polster <max.polster@id-p.de>
2017-12-13 14:35:37 +0100

    Added utility for drawing connections in editor gui.

---
+++ Editor/ConnectionEditorGUI.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unicorn {
+	public static class ConnectionEditorGUI {
+		public static void Draw(Connection conn) {
+			GUILayout.Box(conn.ToString());
+		}
+
+		public static void Draw(IEnumerable<Connection> conns) {
+			foreach (var conn in conns) {
+				Draw(conn);
+			}
+		}
+	}
+}
